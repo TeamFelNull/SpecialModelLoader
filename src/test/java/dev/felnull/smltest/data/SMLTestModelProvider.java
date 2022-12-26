45230227a5003c0c692cbf494b77d50870dc5c45
MORIMORI0317 <morimori.0317@outlook.jp>
2022-12-27 01:55:12 +0900

    change license

--- src/test/java/dev/felnull/smltest/data/SMLTestModelProvider.java
+++ src/test/java/dev/felnull/smltest/data/SMLTestModelProvider.java
@@ -4,7 +4,6 @@ import dev.felnull.smltest.SMLTest;
 import dev.felnull.smltest.block.SMLTestBlocks;
 import dev.felnull.smltest.item.SMLTestItems;
 import dev.felnull.specialmodelloader.api.data.SpecialModelDataGenHelper;
-import dev.felnull.specialmodelloader.impl.SpecialModelLoader;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.models.BlockModelGenerators;
@@ -13,16 +12,12 @@ import net.minecraft.data.models.blockstates.MultiVariantGenerator;
 import net.minecraft.data.models.blockstates.Variant;
 import net.minecraft.data.models.blockstates.VariantProperties;
 import net.minecraft.data.models.model.ModelLocationUtils;
-import net.minecraft.data.models.model.ModelTemplate;
 import net.minecraft.data.models.model.ModelTemplates;
 import net.minecraft.data.models.model.TexturedModel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 
-import java.util.Optional;
-
 public class SMLTestModelProvider extends FabricModelProvider {
-    public static final ModelTemplate OBJ = new ModelTemplate(Optional.of(new ResourceLocation(SpecialModelLoader.MODID, "builtin/obj")), Optional.empty());
 
     public SMLTestModelProvider(FabricDataOutput output) {
         super(output);
@@ -49,5 +44,6 @@ public class SMLTestModelProvider extends FabricModelProvider {
     public void generateItemModels(ItemModelGenerators itemModelGenerator) {
         itemModelGenerator.generateFlatItem(SMLTestItems.NORMAL_MODEL_ITEM, ModelTemplates.FLAT_ITEM);
         SpecialModelDataGenHelper.generateObjModel(SMLTestItems.OBJ_MODEL_ITEM, new ResourceLocation(SMLTest.MODID, "models/item/sea_cheken_pack/sea_chicken.obj"), true, false, null, itemModelGenerator.output);
+
     }
 }
